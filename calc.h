9cd168f972c96e9b65cb4f3bc2ca1dbe9e0cd5f0
jeremysid <jeremysid123@gmail.com>
2023-02-01 11:33:13 +0800

    calc

---
+++ calc.h
@@ -0,0 +1,8 @@
+#ifndef CALC_H
+#define CALC_H
+
+int add(float x, float y) {
+	return x + y;
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include "calc.h"
 
 int main() {
 
 	printf("LMAOO\n");
+	int	res = add(8.6, 9);	
+	printf("%i", res);
 	return 0;
 }
